f46245a83c7dcaa6651e9425268aad1be0ab24c9
soundarya <soundarya.advart@gmail.com>
2025-03-17 16:12:56 +0530

    config added

--- config/sheets_config.py
+++ config/sheets_config.py
@@ -3,5 +3,9 @@ LIST_ID_TO_SHEET_URL_MAP = {
     '901605050377': 'https://docs.google.com/spreadsheets/d/1X-FQykDRUupnc7Imrs6gqPQBqk1-0z2ur8P_Uo7Gh9E/edit',
     '901606186307': 'https://docs.google.com/spreadsheets/d/1eMA70oxzq37IgxiMlOGYQLhUHmNTJ83Myan6fS-1C6Q/edit',
     '901606186317': 'https://docs.google.com/spreadsheets/d/1i1NLz7njbY6dxxMRVKlryqq_Wment_6-2DO6aPOa79w/edit',
-    '901606186318': 'https://docs.google.com/spreadsheets/d/1X-FQykDRUupnc7Imrs6gqPQBqk1-0z2ur8P_Uo7Gh9E/edit',
+    '901606186318': 'https://docs.google.com/spreadsheets/d/1m8EnjaO6MVOtG86cUoh9LM6Fx85PxdyWQLPNuxHnYGw/edit',
+    '901606186180': 'https://docs.google.com/spreadsheets/d/1U28ON-v0DqWUiHNlWKb28FVnh--4MzsrctUsiz2BDT4/edit',
+    '901606186297': 'https://docs.google.com/spreadsheets/d/1bJhddXGz_Ac1oDtqFA0ra0U0jffsR2-I0fjdwVEmj2M/edit',
+    '901606186300': 'https://docs.google.com/spreadsheets/d/13z_p1PRz7jVbTXhG44lPLe0gAyRuPy2RZO62lDKEQDA/edit',
+    '901606186304': 'https://docs.google.com/spreadsheets/d/1dAiKx9n34MaT4z8eojCjhoEU_-lGrN-p0BuysW1Vbj8/edit',
 }
